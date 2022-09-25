139998467c2bb392c5d75d993f31f6c39f3e2828
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-09-25 13:07:11 +0530

    End Test

--- app/src/main/java/com/ravi/vchat/SplashScreen.java
+++ app/src/main/java/com/ravi/vchat/SplashScreen.java
@@ -14,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //send to main activity
+        //this is splash screen
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
